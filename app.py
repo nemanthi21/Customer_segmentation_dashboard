d55657312cbb9a823aa19a6c125ce0ddc99b2f87
nemanthi21 <sisirapremalal26@gmail.com>
2026-02-07 19:33:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import plotly
 
 
 st.set_page_config(
@@ -116,7 +117,7 @@ if has_segment:
             values='Count',
             names='Segment',
             title='Segment Distribution',
-            color_discrete_sequence=px.colors.qualitative.Set3
+           color_discrete_sequence=plotly.colors.qualitative.Set3
 
         )
         st.plotly_chart(fig_pie , use_container_width=True)
@@ -129,7 +130,7 @@ if has_segment:
             y='Count',
             title='Number of customers by segment',
             color='Segment',
-            color_discrete_sequence=px.colors.qualitative.Set2
+            color_discrete_sequence=plotly.colors.qualitative.Set2
         )
         st.plotly_chart(fig_bar , use_container_width=True)
 
